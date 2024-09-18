fee959d612a5f971b0b99e0ee9c85eae269bd461
RamienM <rubenduque12345@gmail.com>
2024-09-18 12:28:30 +0200

    Added MySQL server

--- src/main/java/org/prueba/calificacionesh2/ColegiosAPIApplication.java
+++ src/main/java/org/prueba/calificacionesh2/ColegiosAPIApplication.java
@@ -2,13 +2,12 @@ package org.prueba.calificacionesh2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CalificacionesH2Application {
+public class ColegiosAPIApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalificacionesH2Application.class, args);
+        SpringApplication.run(ColegiosAPIApplication.class, args);
     }
 
 }
